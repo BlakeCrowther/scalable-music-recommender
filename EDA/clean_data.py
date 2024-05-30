0a919ce6ba31c175e8033f1cb553c23e710eb06d
Jennifer Du <jhd002@ucsd.edu>
2024-05-30 00:52:33 -0700

    fix clean_data

--- EDA/clean_data.py
+++ EDA/clean_data.py
@@ -3,22 +3,15 @@ def cleaned_df(song_ratings, song_attributes, genre_hierarchy):
     song_ratings = song_ratings.withColumnRenamed("_c0", "user_id") \
         .withColumnRenamed("_c1", "song_id") \
         .withColumnRenamed("_c2", "rating") 
-    song_attributes = song_attributes.withColumn("song_id", split(song_attributes.value, "\t")[0]) \
-       .withColumn("album_id", split(song_attributes.value, "\t")[1]) \
-       .withColumn("artist_id", split(song_attributes.value, "\t")[2]) \
-       .withColumn("genre_id", split(song_attributes.value, "\t")[3]) \
-       .drop("value")
-    song_attributes = song_attributes.withColumn("song_id", col("song_id").cast("integer")) \
-       .withColumn("album_id", col("album_id").cast("integer")) \
-       .withColumn("artist_id", col("artist_id").cast("integer"))
-    genre_hierarchy = genre_hierarchy.withColumn("genre_id", split(genre_hierarchy.value, "\t")[0]) \
-       .withColumn("parent_genre_id", split(genre_hierarchy.value, "\t")[1]) \
-       .withColumn("level", split(genre_hierarchy.value, "\t")[2]) \
-       .withColumn("genre_name", split(genre_hierarchy.value, "\t")[3]) \
-       .drop("value")
-    genre_hierarchy = genre_hierarchy.withColumn("genre_id", col("genre_id").cast("integer")) \
-       .withColumn("parent_genre_id", col("parent_genre_id").cast("integer")) \
-       .withColumn("level", col("level").cast("integer"))
+    song_attributes = song_attributes.withColumnRenamed("_c0", "song_id") \
+        .withColumnRenamed("_c1", "album_id") \
+        .withColumnRenamed("_c2", "artist_id") \
+        .withColumnRenamed("_c3", "genre_id")
+
+    genre_hierarchy = genre_hierarchy.withColumnRenamed("_c0", "genre_id") \
+        .withColumnRenamed("_c1", "parent_genre_id") \
+        .withColumnRenamed("_c2", "level") \
+        .withColumnRenamed("_c3", "genre_name")
 
     df = song_ratings.join(song_attributes, "song_id", how="inner")
     df = df.join(genre_hierarchy,"genre_id", how="inner")
